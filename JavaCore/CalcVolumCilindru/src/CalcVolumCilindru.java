d5f813dbec4dcfb47df674ec52026c62c05c9dfb
ambreea <160909094+ambreea@users.noreply.github.com>
2024-03-04 12:35:02 +0200

    Delete JavaCore directory

--- JavaCore/CalcVolumCilindru/src/CalcVolumCilindru.java
+++
@@ -1,9 +0,0 @@
-public class CalcVolumCilindru {
-    public static void main(String[] args) {
-        double r = 10;
-        double h = 50;
-        double PI = 3.14;
-        double V = r*r*PI*h;
-        System.out.println(V);
-    }
-}
